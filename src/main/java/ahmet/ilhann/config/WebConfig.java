a48b36c4d0b5cc20b9da85fdad6b4c75ff9e5774
ahmetilhann <ahmet.ddu@gmail.com>
2021-01-02 17:39:42 +0300

    html

--- src/main/java/ahmet/ilhann/config/WebConfig.java
+++ src/main/java/ahmet/ilhann/config/WebConfig.java
@@ -1,14 +1,19 @@
 package ahmet.ilhann.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.UrlBasedViewResolver;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
 
 @EnableWebMvc
 @Configuration
@@ -19,13 +24,28 @@ public class WebConfig implements WebMvcConfigurer {
 
     private static final String RESOLVER_SUFFIX = ".html";
 
+    @Autowired
+    private WebApplicationContext context;
+
     @Bean
-    public ViewResolver viewResolver() {
+    public ServletContextTemplateResolver templateResolver() {
+        ServletContextTemplateResolver resolver = new ServletContextTemplateResolver(context.getServletContext());
+        resolver.setPrefix(RESOLVER_PREFIX);
+        resolver.setSuffix(RESOLVER_SUFFIX);
+        return resolver;
+    }
 
-        UrlBasedViewResolver viewResolver = new InternalResourceViewResolver();
-        viewResolver.setPrefix(RESOLVER_PREFIX);
-        viewResolver.setSuffix(RESOLVER_SUFFIX);
+    @Bean
+    public SpringTemplateEngine templateEngine() {
+        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver());
+        return templateEngine;
+    }
 
+    @Bean
+    public ThymeleafViewResolver thymeleafViewResolver() {
+        ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+        viewResolver.setTemplateEngine(templateEngine());
         return viewResolver;
     }
 
--- src/main/java/ahmet/ilhann/controller/HomeController.java
+++ src/main/java/ahmet/ilhann/controller/HomeController.java
@@ -1,14 +1,41 @@
 package ahmet.ilhann.controller;
 
+import ahmet.ilhann.model.Tour;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class HomeController {
 
+    private List<Tour> tours = new ArrayList<>();
+
     @GetMapping("/")
     public String getHome() {
         return "home";
+    }
+
+    @GetMapping("/addTour")
+    public String addTour(Model model) {
+        model.addAttribute("tour", new Tour());
+        return "form";
+    }
+
+    @PostMapping("/processForm")
+    public String processForm(@ModelAttribute Tour tour) {
+        tours.add(tour);
+        return "redirect:showOffer";
+    }
+
+    @GetMapping("/showOffer")
+    public String showOffer(Model model) {
+        model.addAttribute("tours", tours);
+        return "tours";
     }
 }
